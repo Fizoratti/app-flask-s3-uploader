ca7fbc5140668b06e7b31be82af22247557397a7
Felipe <32341457+Fizoratti@users.noreply.github.com>
2021-07-01 21:53:02 -0300

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y python2.7 python-pip
+sudo apt-get install -y virtualenv python2.7 python-pip
 virtualenv venv
 pip install -r requirements.txt
