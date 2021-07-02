0e61c10f695037e30bcf548c2e2e06e1c2489689
Felipe <32341457+Fizoratti@users.noreply.github.com>
2021-07-01 21:33:47 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ VIRTUALENV=/home/ubuntu/venv
 
 pushd $(dirname "$0")
 source $VIRTUALENV/bin/activate
+pip install -r requirements.txt
 export FLASK_APP=app.py
 flask run --host=0.0.0.0 --port 8080
 deactivate
